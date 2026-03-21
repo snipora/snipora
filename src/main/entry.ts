1feb93f1a953f4f0159330f5019ca9b8f02831e4
Barakudum <68517540+Barakudum@users.noreply.github.com>
2026-03-21 12:19:36 +0100

    frontend: added console-logging for critical errors

--- src/main/entry.ts
+++ src/main/entry.ts
@@ -1,9 +1,16 @@
 import "@/index.css";
 import "@/forward-console.ts";
-import { createApp } from "vue";
+import {App, createApp} from "vue";
 import i18n from "@/i18n";
 import MainApp from "./MainApp.vue";
 
 createApp(MainApp)
     .use(i18n)
+    .use({
+      install: (app: App) => {
+        app.config.errorHandler = (err, instance, info) => {
+          console.error(err, instance, info);
+        }
+      },
+    })
     .mount("#app");
--- src/popup/entry.ts
+++ src/popup/entry.ts
@@ -1,9 +1,16 @@
 import "@/index.css";
 import "@/forward-console.ts";
-import { createApp } from "vue";
+import {App, createApp} from "vue";
 import i18n from "@/i18n";
 import PopupApp from "./PopupApp.vue";
 
 createApp(PopupApp)
     .use(i18n)
+    .use({
+      install: (app: App) => {
+        app.config.errorHandler = (err, instance, info) => {
+          console.error(err, instance, info);
+        }
+      },
+    })
     .mount("#app");
