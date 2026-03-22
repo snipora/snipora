64a193c86b8173b124a4f7956c21eb226a4d611c
Barakudum <68517540+Barakudum@users.noreply.github.com>
2026-03-22 13:37:30 +0100

    frontend fixed command api-functions names
    - missing `^invoke*` prefix for a lot of api-functions

--- src/api/commands/snippets.ts
+++ src/api/commands/snippets.ts
@@ -6,7 +6,7 @@ import {invoke} from "@tauri-apps/api/core";
  * creates a new snippet
  * @param snippet new snippet data
  */
-export async function createSnippet(
+export async function invokeCreateSnippet(
     snippet: Pick<SnippetDto, "label" | "snippet" | "tags">,
 ): Promise<SnippetDto["id"]> {
   return invoke<SnippetDto["id"]>("create_snippet", snippet);
@@ -16,7 +16,7 @@ export async function createSnippet(
  * update an existing snippet
  * @param snippet updated snippet data
  */
-export async function updateSnippet(
+export async function invokeUpdateSnippet(
     snippet: Pick<SnippetDto, "id" | "label" | "snippet" | "tags">,
 ): Promise<void> {
   return invoke<void>("update_snippet", snippet);
@@ -26,14 +26,14 @@ export async function updateSnippet(
  * deletes an existing snippet
  * @param snippetId id of the snippet to delete
  */
-export async function deleteSnippet(snippetId: SnippetDto["id"]): Promise<void> {
+export async function invokeDeleteSnippet(snippetId: SnippetDto["id"]): Promise<void> {
   return invoke<void>("delete_snippet", { snippetId });
 }
 
 /**
  * returns all snippets
  */
-export async function getAllSnippets(): Promise<SnippetDto[]> {
+export async function invokeGetAllSnippets(): Promise<SnippetDto[]> {
   return invoke<SnippetDto[]>("get_all_snippets");
 }
 
@@ -41,6 +41,6 @@ export async function getAllSnippets(): Promise<SnippetDto[]> {
  * get up to `limit` of recently used snippets
  * @param limit maximum number of snippets
  */
-export async function getRecentSnippets(limit: number): Promise<SnippetDto[]> {
+export async function invokeGetRecentSnippets(limit: number): Promise<SnippetDto[]> {
   return invoke<SnippetDto[]>("get_recent_snippets", { limit });
 }
--- src/api/commands/tags.ts
+++ src/api/commands/tags.ts
@@ -4,6 +4,6 @@ import {invoke} from "@tauri-apps/api/core";
 /**
  * return a list of all tags available
  */
-export async function getAllTags(): Promise<Tag[]> {
+export async function invokeGetAllTags(): Promise<Tag[]> {
   return invoke<Tag[]>("get_all_snippets");
 }
