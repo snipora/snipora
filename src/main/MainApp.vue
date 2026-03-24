e2299b9f96a42c379537ff0d59a8b2e20280dd11
Barakudum <68517540+Barakudum@users.noreply.github.com>
2026-03-24 22:01:33 +0100

    added `<ScrollArea>` to `<main>`-component for main-window
    - wrapped `<main>`-component in `<ScrollArea>`
    - added proper positioning, max-width, and padding for `<main>`

--- src/main/MainApp.vue
+++ src/main/MainApp.vue
@@ -3,6 +3,7 @@ import {AppSidebar} from "@/main/sidebar";
 import {SidebarProvider} from "@/components/ui/sidebar";
 import {provide, ref} from "vue";
 import {INJECTION_KEY_MAIN_VIEW, ViewState, VIEW_TO_COMPONENT} from "@/main/views";
+import {ScrollArea} from "@/components/ui/scroll-area";
 
 const viewState = ref<ViewState>({
   id: "all-snippets",
@@ -14,8 +15,10 @@ provide(INJECTION_KEY_MAIN_VIEW, viewState);
 <template>
   <SidebarProvider>
     <AppSidebar />
-    <main>
-      <component :is="VIEW_TO_COMPONENT[viewState.id]" v-bind="viewState" />
-    </main>
+    <ScrollArea class="h-svh w-full">
+      <main class="mx-auto max-w-3xl p-4">
+        <component :is="VIEW_TO_COMPONENT[viewState.id]" v-bind="viewState" />
+      </main>
+    </ScrollArea>
   </SidebarProvider>
 </template>
