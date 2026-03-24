5a4d053e20433409b29a2c0c18d8b640431bd04c
Barakudum <68517540+Barakudum@users.noreply.github.com>
2026-03-24 21:47:57 +0100

    added additional `src/components/icons/` based on the icons in `assets/`

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,1 +1,4 @@
+export { default as SniporaLogo } from './SniporaLogo.vue';
+export { default as SniporaIconLight } from './SniporaIconLight.vue';
+export { default as SniporaIconDark } from './SniporaIconDark.vue';
 export { default as LucideTagOff } from './TagOff.vue';
