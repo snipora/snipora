78fa6322bfdc9a187e79ea955b77e5b752b6d5ba
Barakudum <68517540+Barakudum@users.noreply.github.com>
2026-03-26 19:43:58 +0100

    fix: corrected command-name for `invokeGetAllTags`

--- src/api/commands/tags.ts
+++ src/api/commands/tags.ts
@@ -5,5 +5,5 @@ import {invoke} from "@tauri-apps/api/core";
  * return a list of all tags available
  */
 export async function invokeGetAllTags(): Promise<Tag[]> {
-  return invoke<Tag[]>("get_all_snippets");
+  return invoke<Tag[]>("get_all_tags");
 }
