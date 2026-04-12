4b79df6351e73422236e9d84735bf102af8a0e83
Barakudum <68517540+Barakudum@users.noreply.github.com>
2026-04-12 18:34:48 +0200

    feat: implemented "duplicate snippet" action

--- src/main/components/snippet-list/SnippetListEntry.vue
+++ src/main/components/snippet-list/SnippetListEntry.vue
@@ -8,38 +8,42 @@ import {
   SnippetCardSnippet,
   SnippetCardTags
 } from "@/main/components/snippet-card";
-import {invokeDeleteSnippet} from "@/api/commands";
+import {invokeCreateSnippet, invokeDeleteSnippet} from "@/api/commands";
+import {useAsyncAction} from "@/composables/useAsyncAction.ts";
+import {Spinner} from "@/components/ui/spinner";
 
 const props = defineProps<{
   snippet: SnippetDto
 }>();
 
-function handleEdit() {
+const { invoke: handleDuplicate, isRunning: isDuplicating } = useAsyncAction(async () => {
+  await invokeCreateSnippet({
+    label: props.snippet.label,
+    snippet: props.snippet.snippet,
+    tags: props.snippet.tags,
+  });
+});
 
-}
-
-function handleDuplicate() {
-
-}
-
-async function handleDelete() {
+const { invoke: handleDelete, isRunning: isDeleting } = useAsyncAction(async () => {
   await invokeDeleteSnippet(props.snippet.id);
-}
+});
 </script>
 
 <template>
   <SnippetCard>
     <SnippetCardHeader :label="snippet.label" />
     <SnippetCardActions>
-      <SnippetCardAction @click="handleEdit">
+      <SnippetCardAction>
         <LucidePencil />
       </SnippetCardAction>
-      <SnippetCardAction @click="handleDuplicate">
-        <LucideCopy />
+      <SnippetCardAction :disabled="isDuplicating" @click="handleDuplicate">
+        <Spinner v-if="isDuplicating" />
+        <LucideCopy v-else />
       </SnippetCardAction>
       <!-- todo: dialog for confirmation -->
-      <SnippetCardAction class="hover:text-destructive" @click="handleDelete">
-        <LucideTrash />
+      <SnippetCardAction class="hover:text-destructive" :disabled="isDeleting" @click="handleDelete">
+        <Spinner v-if="isDeleting" />
+        <LucideTrash v-else />
       </SnippetCardAction>
     </SnippetCardActions>
     <SnippetCardTags :tags="snippet.tags" />
