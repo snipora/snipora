e76e2656e5c77d9e8dac4b1755039aa9f3fda273
Barakudum <68517540+Barakudum@users.noreply.github.com>
2026-04-12 20:00:41 +0200

    feat: added confirmation dialog before deleting a snippet

--- src/main/components/snippet-list/SnippetListEntry.vue
+++ src/main/components/snippet-list/SnippetListEntry.vue
@@ -1,20 +1,33 @@
 <script setup lang="ts">
+import {ref} from "vue";
 import {SnippetDto} from "@/api/dto.ts";
 import {LucideCopy, LucidePencil, LucideTrash} from "@lucide/vue";
 import {
-  SnippetCard, SnippetCardAction,
+  SnippetCard,
+  SnippetCardAction,
   SnippetCardActions,
   SnippetCardHeader,
   SnippetCardSnippet,
   SnippetCardTags
 } from "@/main/components/snippet-card";
+import {
+  Dialog,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogScrollContent,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {Button} from "@/components/ui/button";
+import {Spinner} from "@/components/ui/spinner";
 import {invokeCreateSnippet, invokeDeleteSnippet} from "@/api/commands";
 import {useAsyncAction} from "@/composables/useAsyncAction.ts";
-import {Spinner} from "@/components/ui/spinner";
 
 const props = defineProps<{
   snippet: SnippetDto
 }>();
+
+const isDeleteDialogOpen = ref(false);
 
 const { invoke: handleDuplicate, isRunning: isDuplicating } = useAsyncAction(async () => {
   await invokeCreateSnippet({
@@ -26,6 +39,7 @@ const { invoke: handleDuplicate, isRunning: isDuplicating } = useAsyncAction(asy
 
 const { invoke: handleDelete, isRunning: isDeleting } = useAsyncAction(async () => {
   await invokeDeleteSnippet(props.snippet.id);
+  isDeleteDialogOpen.value = false;
 });
 </script>
 
@@ -40,12 +54,27 @@ const { invoke: handleDelete, isRunning: isDeleting } = useAsyncAction(async () 
         <Spinner v-if="isDuplicating" />
         <LucideCopy v-else />
       </SnippetCardAction>
-      <!-- todo: dialog for confirmation -->
-      <SnippetCardAction class="hover:text-destructive" :disabled="isDeleting" @click="handleDelete">
-        <Spinner v-if="isDeleting" />
-        <LucideTrash v-else />
+      <SnippetCardAction class="hover:text-destructive" @click="isDeleteDialogOpen = true">
+        <LucideTrash />
       </SnippetCardAction>
     </SnippetCardActions>
+    <Dialog v-model:open="isDeleteDialogOpen">
+      <DialogScrollContent>
+        <DialogHeader>
+          <DialogTitle>{{ $t("dialogs.delete-snippet.dialog-title") }}</DialogTitle>
+          <DialogDescription>{{ $t("dialogs.delete-snippet.dialog-description") }}</DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <Button variant="outline" @click="isDeleteDialogOpen = false">
+            {{ $t("dialogs.delete-snippet.cancel") }}
+          </Button>
+          <Button variant="destructive" :disabled="isDeleting" @click="handleDelete">
+            <Spinner v-if="isDeleting" />
+            {{ $t("dialogs.delete-snippet.confirm") }}
+          </Button>
+        </DialogFooter>
+      </DialogScrollContent>
+    </Dialog>
     <SnippetCardTags :tags="snippet.tags" />
     <SnippetCardSnippet :snippet="snippet.snippet" show-copy />
   </SnippetCard>
