1fdbd970d4e51a9b1e2872d1aacc5cddba91a4f8
Barakudum <68517540+Barakudum@users.noreply.github.com>
2026-04-15 10:17:44 +0200

    feat: useSmartPopupHeight.ts replaced debounced with throttle

--- src/popup/composables/useSmartPopupHeight.ts
+++ src/popup/composables/useSmartPopupHeight.ts
@@ -1,11 +1,11 @@
-import {createSharedComposable, useElementSize, watchDebounced} from "@vueuse/core";
+import {createSharedComposable, useElementSize, watchThrottled} from "@vueuse/core";
 import {invokePopupAdjustHeight} from "@/api/commands";
 
 
 export const useSmartPopupHeight = createSharedComposable(() => {
   const { height: documentHeight } = useElementSize(document.documentElement);
 
-  watchDebounced(documentHeight, async (preferredHeight) => {
+  watchThrottled(documentHeight, async (preferredHeight) => {
     await invokePopupAdjustHeight(preferredHeight);
-  }, { debounce: 50 });
+  }, { throttle: 50 });
 });
