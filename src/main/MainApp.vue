95bff08534281f4b2ffa324fe127dd0e487ba761
Barakudum <68517540+Barakudum@users.noreply.github.com>
2026-04-17 13:21:31 +0200

    feat: added memory to main-window view-state

--- src/main/MainApp.vue
+++ src/main/MainApp.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
 import {AppSidebar} from "@/main/sidebar";
 import {SidebarProvider} from "@/components/ui/sidebar";
-import {provide, ref} from "vue";
+import {provide} from "vue";
 import {INJECTION_KEY_MAIN_VIEW, ViewState, VIEW_TO_COMPONENT} from "@/main/views";
 import {ScrollArea} from "@/components/ui/scroll-area";
-import {useColorMode} from "@vueuse/core";
+import {useColorMode, useLocalStorage} from "@vueuse/core";
 import {useTauriEventListener} from "@/composables/useTauriEventListener.ts";
 
 useColorMode({ writeDefaults: false });
 
-const viewState = ref<ViewState>({
+const viewState = useLocalStorage<ViewState>('view-state', () => ({
   id: "all-snippets",
-});
+}), { listenToStorageChanges: false });
 
 provide(INJECTION_KEY_MAIN_VIEW, viewState);
 
