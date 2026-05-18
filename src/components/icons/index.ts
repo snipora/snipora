318c5130ae9d9fba6f234d69e7c5c2fea45d1cb3
Barakudum <68517540+Barakudum@users.noreply.github.com>
2026-05-18 10:10:32 +0200

    feat: cleanup of frontend custom icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,4 +1,12 @@
+import {createLucideIcon} from "@lucide/vue";
+
 export { default as SniporaLogo } from './SniporaLogo.vue';
 export { default as SniporaIconLight } from './SniporaIconLight.vue';
 export { default as SniporaIconDark } from './SniporaIconDark.vue';
-export { default as LucideTagOff } from './TagOff.vue';
+
+export const LucideTagOff = createLucideIcon("tag-off", [
+  ["path", { d: "m14.172 4.172-1.586-1.586A2 2 0 0 0 11.172 2H4a2 2 0 0 0-2 2v7.172a2 2 0 0 0 .586 1.414l1.586 1.586" }],
+  ["path", { d: "M2 22 22 2" }],
+  ["path", { d: "m7 17 4.29 4.29a2.426 2.426 0 0 0 3.42 0l6.58-6.58a2.426 2.426 0 0 0 0-3.42L17 7" }],
+  ["circle", { cx: 7.5, cy: 7.5, r: .5, fill: "currentColor" }],
+]);
