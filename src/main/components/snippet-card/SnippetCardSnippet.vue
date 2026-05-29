690da35e3789f141a5d70f78d6c3e31e9d409a25
Barakudum <68517540+Barakudum@users.noreply.github.com>
2026-05-29 11:49:54 +0200

    feat(frontend): snippets are now clamped to 5 lines; can be expanded

--- src/main/components/snippet-card/SnippetCardSnippet.vue
+++ src/main/components/snippet-card/SnippetCardSnippet.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
-import {HTMLAttributes} from "vue";
+import {computed, HTMLAttributes, ref} from "vue";
 import {cn} from "@/lib/utils.ts";
 import {Button} from "@/components/ui/button";
-import {LucideClipboardCheck, LucideClipboardCopy} from "@lucide/vue";
+import {LucideClipboardCheck, LucideClipboardCopy, LucideFoldVertical, LucideUnfoldVertical} from "@lucide/vue";
 import {useClipboard} from "@vueuse/core";
 
 const props = defineProps<{
   class?: HTMLAttributes["class"]
   snippet: string
 }>();
+
+const canExpand = computed(() => props.snippet.split(/\r\n|\r|\n/).length >= 5);
+const isExpanded = ref<boolean>(false);
 
 const { copy: copyToClipboard, copied: recentlyCopied } = useClipboard({
   source: () => props.snippet
@@ -24,10 +27,19 @@ async function handleCopy() {
       data-slot="snippet-card-snippet"
       class="relative overflow-hidden"
   >
-    <Button variant="ghost" size="icon-sm" class="bg-secondary opacity-0 group-focus-within/snippet-card:opacity-100 group-hover/snippet-card:opacity-100 transition-opacity absolute top-0 right-0 cursor-pointer" @click="handleCopy">
-      <LucideClipboardCheck v-if="recentlyCopied" />
-      <LucideClipboardCopy v-else />
-    </Button>
-    <pre :class="cn('font-mono bg-secondary text-secondary-foreground px-2 py-1 rounded-md inset-shadow-xs overflow-x-scroll', props.class)">{{ snippet }}</pre>
+    <div class="absolute top-0 right-0 flex flex-row-reverse bg-secondary opacity-0 group-focus-within/snippet-card:opacity-100 group-hover/snippet-card:opacity-100 transition-opacity">
+      <Button variant="ghost" size="icon-sm" class="cursor-pointer" @click="handleCopy">
+        <LucideClipboardCheck v-if="recentlyCopied" />
+        <LucideClipboardCopy v-else />
+      </Button>
+      <Button v-if="canExpand" variant="ghost" size="icon-sm" class="cursor-pointer" @click="isExpanded = !isExpanded">
+        <LucideFoldVertical v-if="isExpanded" />
+        <LucideUnfoldVertical v-else />
+      </Button>
+    </div>
+    <pre
+        :data-expanded="isExpanded"
+        :class="cn('font-mono bg-secondary text-secondary-foreground px-2 py-1 rounded-md inset-shadow-xs overflow-x-scroll! line-clamp-5 data-[expanded=true]:line-clamp-none', props.class)"
+    >{{ snippet }}</pre>
   </div>
 </template>
